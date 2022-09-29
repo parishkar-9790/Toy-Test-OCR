b42f70fda73bbcdcbcd0a38cccc6c2277e01f150
parishkar singh <parishkar.work@gmail.com>
2022-09-29 22:09:31 +0530

    added so many things no changleog this time

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request,redirect,url_for
+import os
+UPLOAD_FOLDER=os.getcwd()+'\\upload'
 app = Flask(__name__)
-
-
+from user import routes
+@app.route('/createAccount')
+def createAccount():
+    return render_template('createAcc.html')
 @app.route('/')
 def doLogin():
     return render_template('login.html')
 @app.route('/index')
 def upload():
-    return render_template("uploadRes.html")
-# def hello_world():  # put application's code here
-#     # doLogin()
-#     # return 'Hello World!'
-
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
